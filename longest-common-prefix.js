618ee2ef471420fa4ce016d906d30caa61d67644
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-15 12:56:14 -0400

    leetcode problems

---
+++ longest-common-prefix.js
@@ -0,0 +1,11 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+let strs = ["flower","flow","flight"]
+var longestCommonPrefix = function(strs) {
+    x=0
+    for (let i = 0; i < strs.length; i++ ){
+        
+    }
+};
+
